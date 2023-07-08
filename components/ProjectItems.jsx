b137f84be47ecd7a29c3bfdd933790917034da9f
CJ Sabijon <cjsabijon@gmail.com>
2023-07-08 15:53:14 +0800

    Fixed project hover.

--- components/ProjectItems.jsx
+++ components/ProjectItems.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const ProjectItems = ({title, backgroundImg, projectStack, projectUrl}) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full 
-    shadow-md shadow-gray-300 dark:shadow-shadow-dark rounded-md group h
-    over:bg-gradient-to-r from-blue-500 to-cyan-500">
+    shadow-md shadow-gray-300 dark:shadow-shadow-dark rounded-md 
+    group hover:bg-gradient-to-r from-blue-500 to-cyan-500">
         <Image className="rounded-md group-hover:opacity-10 ease-in duration-200" src={backgroundImg} width='2000' height='1333' alt=""/>
             <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
                 <h5 className="text-white tracking-wider text-center text-3xl lg:text-2xl">{title}</h5>
